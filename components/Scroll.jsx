6ae3641a89c9cb6d55c171db440651b8907471f9
Sumit Kumar Verma <sumit2004ku@gmail.com>
2025-07-18 16:43:56 +0530

    one_step_closer

--- components/Scroll.jsx
+++ components/Scroll.jsx
@@ -39,6 +39,9 @@ const Scroll = () => {
                     }}
                 ></path>
         </svg>
+        <span className="home__scroll-name">Scroll Down</span>
+
+        <i class="uil uil-arrow-down home__scroll-arrow"></i>
         </a>
     </div>
   )
